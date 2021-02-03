e771ae3f17f9627eab5daec4f279de67bce0c19c
Federico-Benitez <fedebenitez1996@gmail.com>
2021-02-03 19:59:35 -0300

    Validaciones de los input agregada

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,3 +1,6 @@
+const bcrypt = require("bcryptjs");
+const { UserInputError } = require("apollo-server");
+
 const { User } = require("../models");
 
 module.exports = {
@@ -13,23 +16,54 @@ module.exports = {
   },
   Mutation: {
     register: async (_, args) => {
-      const { username, email, password, confirmPassword } = args;
+      let { username, email, password, confirmPassword } = args;
+      let errors = {};
       try {
-        //TODO: Validar input
+        //Validar input
+        if (email.trim() === "") {
+          errors.email = "El email no puede estar vacio";
+        }
+        if (username.trim() === "") {
+          errors.username = "El username no puede estar vacio";
+        }
+        if (password.trim() === "") {
+          errors.password = "El password no puede estar vacio";
+        }
+        if (confirmPassword.trim() === "") {
+          errors.confirmPassword =
+            "La confirmacion de la contraseña no puede estar vacia";
+        }
+        if (password !== confirmPassword) {
+          errors.password = "No coincide con la confirmacion de contraseña";
+          errors.confirmPassword = "No coincide con la contraseña";
+        }
 
-        //TODO: Comprobar si el username/ email existe
+        //Comprobar si el username/ email existe
+        const userByUsername = await User.findOne({ where: { username } });
+        const userByEmail = await User.findOne({ where: { email } });
 
-        //TODO: Crear usuario
+        if (userByUsername) errors.username = "El nombre de usuario ya existe";
+        if (userByEmail) errors.email = "Ya existe una cuenta con este email";
+
+        //Comprobar si existen errores
+        if (Object.keys(errors).length > 0) {
+          throw errors;
+        }
+
+        //Encriptar contraseña
+        password = await bcrypt.hash(password, 6);
+
+        //Crear usuario
         const user = await User.create({
           username,
           email,
           password
         });
-        //TODO: Return user
+        //Return user
         return user;
       } catch (err) {
         console.log(err);
-        throw err;
+        throw new UserInputError("Bad input", { errors: err });
       }
     }
   }
