c8b3af29143c1a0bb36dc6228887bf84dcfeeea0
Federico-Benitez <fedebenitez1996@gmail.com>
2021-02-05 18:15:28 -0300

    Mensajes estilizados

---
+++ client/src/pages/home/Message.js
@@ -0,0 +1,41 @@
+import React from "react";
+import classNames from "classnames";
+import { useAuthState } from "../../context/auth";
+import moment from "moment";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
+
+export default function Message({ message }) {
+  const { user } = useAuthState();
+  const sent = message.from === user.username;
+  const recieved = !sent;
+
+  return (
+    <OverlayTrigger
+      placement={sent ? "right" : "left"}
+      overlay={
+        <Tooltip>
+          {moment(message.createdAt).format("MMMM DD, YYYY @ h:mm a")}
+        </Tooltip>
+      }
+    >
+      <div
+        className={classNames("d-flex my-1", {
+          "ml-auto": sent,
+          "mr-auto": recieved
+        })}
+      >
+        <div
+          className={classNames("py-2 px-3 rounded-pill", {
+            "bg-primary": sent,
+            "bg-secondary": recieved
+          })}
+        >
+          <p className={classNames({ "text-white": sent })} key={message.uuid}>
+            {message.content}
+          </p>
+          {/* <p>{moment(message.createdAt).format("h:mm a")}</p> */}
+        </div>
+      </div>
+    </OverlayTrigger>
+  );
+}
--- client/src/pages/home/Messages.js
+++ client/src/pages/home/Messages.js
@@ -1,10 +1,10 @@
-import React from "react";
-import { useEffect } from "react";
+import React, { useEffect, Fragment } from "react";
 
 import { gql, useLazyQuery } from "@apollo/client";
 import { Col } from "react-bootstrap";
 
 import { useMessageDispatch, useMessageState } from "../../context/message";
+import Message from "./Message";
 
 const GET_MESSAGES = gql`
   query getMessages($from: String!) {
@@ -54,14 +54,27 @@ export default function Messages() {
   } else if (messagesLoading) {
     selectChatMarkup = <p>Cargando..</p>;
   } else if (messages.length > 0) {
-    selectChatMarkup = messages.map((message) => (
-      <p key={message.uuid}>{message.content}</p>
+    selectChatMarkup = messages.map((message, index) => (
+      <Fragment key={message.uuid}>
+        <Message message={message} />
+        {index === messages.length - 1 && (
+          <div className="invisible">
+            <hr className="m-0 " />
+          </div>
+        )}
+      </Fragment>
     ));
   } else if (messages.length === 0) {
     selectChatMarkup = (
-      <p>Aun no tienes una conversacion con esta persona, saludala!</p>
+      <p>
+        Aun no tienes una conversacion con esta persona, enviale un mensaje!
+      </p>
     );
   }
 
-  return <Col xs={8}>{selectChatMarkup}</Col>;
+  return (
+    <Col xs={10} md={8} className="messages-box d-flex flex-column-reverse">
+      {selectChatMarkup}
+    </Col>
+  );
 }
