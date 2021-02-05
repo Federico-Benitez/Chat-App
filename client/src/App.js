5a98c105519ad93435e72c0d0489f09477b098fe
Federico-Benitez <fedebenitez1996@gmail.com>
2021-02-05 07:53:41 -0300

    control de token agregado

--- client/src/App.js
+++ client/src/App.js
@@ -8,18 +8,22 @@ import Register from "./pages/Register";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 
+import { AuthProvider } from "./context/auth";
+
 function App() {
   return (
     <ApolloProvider>
-      <BrowserRouter>
-        <Container className="pt-5">
-          <Switch>
-            <Route path="/registro" component={Register} />
-            <Route exact path="/" component={Home} />
-            <Route path="/login" component={Login} />
-          </Switch>
-        </Container>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Container className="pt-5">
+            <Switch>
+              <Route path="/registro" component={Register} />
+              <Route exact path="/" component={Home} />
+              <Route path="/login" component={Login} />
+            </Switch>
+          </Container>
+        </BrowserRouter>
+      </AuthProvider>
     </ApolloProvider>
   );
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,30 @@
 import React from "react";
+import { Row, Col, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-export default function Home() {
+import { useAuthDispatch } from "../context/auth";
+
+export default function Home({ history }) {
+  const dispatch = useAuthDispatch();
+  const logout = () => {
+    dispatch({
+      type: "LOGOUT"
+    });
+    history.push("/login");
+  };
+
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <Row className="bg-white justify-content-around">
+      <Link to="/login">
+        <Button variant="link">Login</Button>
+      </Link>
+      <Link to="/registro">
+        <Button variant="link">Registro</Button>
+      </Link>
+
+      <Button variant="link" onClick={logout}>
+        Cerrar Sesión
+      </Button>
+    </Row>
   );
 }
