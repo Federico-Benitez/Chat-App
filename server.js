8bfac8999c6e41555356265e11a4b5441e2a0f7d
Federico-Benitez <fedebenitez1996@gmail.com>
2021-02-06 13:08:34 -0300

    cambios para deploy

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const server = new ApolloServer({
   context: contextMiddleware
 });
 
-server.listen().then(({ url }) => {
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 
   sequelize
